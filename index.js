cffe23d58a19266e0f21f3ea654a02ae5371a262
rafaele <rafaele2028@gmail.com>
2024-02-15 17:01:38 -0300

    config route and user

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const userRoute = require("./src/routes/user.route")
+const port = 3000;
+app.use(express.json());
+app.use("/user", userRoute);
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
-app.listen(3000)
+app.listen(port, () => console.log(`servidor rodando na porta ${port}`));
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,25 @@
+
+const create = (req, res) => {
+const {name, username, email, password, avatar, background} = req.body;
+
+if (!name || !username || !email  || !password || !avatar  || !background){
+    res.status(400).send({message: "submit all fields for registration"})
+}
+
+
+res.status(201).send({
+    message: "user created sucessfully",
+    user: {
+        name,
+        username,
+        email,
+        avatar,
+        background,
+
+    },
+    
+});
+
+};
+
+module.exports = { create };
